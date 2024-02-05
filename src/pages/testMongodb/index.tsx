51ad8ffc5ea691175b863675cc6676ade3305bc9
PuffCab <berlinkollner@gmail.com>
2024-02-05 23:12:09 +0100

    connection to mongodb with mongoose boiler plate

--- src/pages/testMongodb/index.tsx
+++ src/pages/testMongodb/index.tsx
@@ -1,20 +1,22 @@
 import HospitalModel from "@/models/hospitalModel";
 import dbConnection from "@/utils/dbConnection";
 import React from "react";
+import mongoDBConnect from "../../../lib/mongoDBConnect";
 
 export async function getServerSideProps() {
   // Fetch data from external API
   //   const res = await fetch(`https://.../data`);
   //   const data = await res.json();
 
-  const mongoDB = await dbConnection();
+  // const mongoDB = await dbConnection();
+  await mongoDBConnect(); //REVIEW here i just replaced your connection function for mine. to make equal to a variable, as you did, or not, as I do, makes no difference here
 
   const hospitals = await HospitalModel.find();
 
   console.log("hospitals :>> ", hospitals);
 
   // Pass data to the page via props
-  return { props: { data } };
+  return { props: { hospitals } };
 }
 
 function testMongodb() {

768ea6c4efdad238b5c00a54c9910935a683c917
PuffCab <berlinkollner@gmail.com>
2024-02-10 17:07:58 +0100

    custom hook to calculate distance between coordinates

--- src/pages/hospitals.jsx
+++ src/pages/hospitals.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ListOfHospitals from "../components/ListOfHospitals";
+import useCalculateDistanceToHospitals from "../utils/usecalculateDistanceToHospitals";
 
 function HospitalsPage() {
+
+  //1.7km according to google maps
+  // const location1 = [52.47304137480423, 13.425472026026231]
+  // const location2 = [52.48656925572065, 13.424486699004747]
+
+
+  //6.4km according to google maps:
+  const location1=[52.51812040534005, 13.432188559032596]
+  const location2=[52.5094117367754, 13.520745861428354]
+
+  const distance = useCalculateDistanceToHospitals(location1, location2)
+
+
+  console.log('distance final :>> ', distance);
   return (
     <div>
       <h1>List of Hospitals</h1>
---
+++ src/utils/useCalculateDistanceToHospitals.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+//REVIEW this custom hook is built using the  Harvestine formula, to calculate distances between locations, taking into account the earth curvature : https://en.wikipedia.org/wiki/Haversine_formula
+
+//this the shape expected in coordinates1/2 is and array with a [latitudeNumber, longitudeNumber]
+
+// function useCalculateDistanceToHospitals(coord1, coord2) {
+function useCalculateDistanceToHospitals(coordinates1, coordinates2) {
+  console.log(coordinates1, coordinates2);
+  const [distance, setDistance] = useState(0);
+  const [lat1, lon1] = coordinates1;
+  const [lat2, lon2] = coordinates2;
+  const isSameCoordinates =
+    lat1 == lon1 && lat2 == lon2;
+
+  const calculateDistance = () => {
+    if (isSameCoordinates) {
+      //if both pair of coordinates has the same values, then there is no distance.
+      setDistance(0);
+      console.log("aqi estamos");
+      // distance = 0
+    }
+    if (!isSameCoordinates) {
+      //Conversion from degrees to Radians. Trigonometric functions in JS expect angles to be in radians
+      const radianLat1 = (Math.PI * lat1) / 180;
+      const radianLat2 = (Math.PI * lat2) / 180;
+
+      // theta is the greek letter used to represent a measured angle. Used here to represen distance between 2 points.
+      const theta = lon1 - lon2;
+      const radianTheta = (Math.PI * theta) / 180;
+
+      //pointsDistance represents the angular distance between two points.
+      let pointsDistance =
+        Math.sin(radianLat1) * Math.sin(radianLat2) +
+        Math.cos(radianLat1) * Math.cos(radianLat2) * Math.cos(radianTheta);
+
+      //making sure that distance value is not bigger than 1, since arc cosine expects a value withih range range [-1, 1].
+      if (pointsDistance > 1) {
+        console.log("if 38");
+        pointsDistance = 1;
+      } else {
+        console.log("else 41");
+        //calculating arc cosine
+        pointsDistance = Math.acos(pointsDistance);
+        //converting radians to degrees
+        pointsDistance = (pointsDistance * 180) / Math.PI;
+        //converting angula distance to nautical miles
+        pointsDistance = pointsDistance * 60 * 1.1515;
+        //nautical miles to KM
+        pointsDistance = pointsDistance * 1.609344;
+        console.log("pointsDistance :>> ", pointsDistance);
+        setDistance(pointsDistance.toFixed(2))
+        
+      }
+    }
+  };
+
+
+
+useEffect(() => {
+calculateDistance()
+}, [coordinates1, coordinates2])
+
+
+  return distance;
+}
+
+export default useCalculateDistanceToHospitals;

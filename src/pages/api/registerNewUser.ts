b8ce3397990d5557d803889b99246b8b6eb02862
helene-abiassi <abiassi.helene@gmail.com>
2024-02-13 15:20:22 +0100

    register work in client and server

--- src/pages/api/registerNewUser.ts
+++ src/pages/api/registerNewUser.ts
@@ -4,76 +4,8 @@ import { hashPassword } from "../../utils/passwordServices"
 import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
 
-
-// const registerNewUser = async (req, res) => {
-//   const dbConnect = await dbConnection();
-
-// try {
-
-//   const {name, email, password} = req.body;
-
-//   if (!email || !password){
-//     res.status(405).json({
-//       message: "You need to provide an email and password"
-//     })
-//   }
-//   // const database =  dbConnect.db("lilu2");
-
-// const existingUser = await dbConnect.collection("userProfiles").find({ email: email })
-
-// // const existingUser = await UsersModel.find({email:email})
-
-// // if (existingUser) {
-// //   res.status(200).json({
-// //     message: "This email already exists in the database "
-// //   })
-// // }
-
-// const hashedPassword = await hashPassword(password);
-
-// if (hashedPassword) {
-
-//   try {
-//     const newUser = new UsersModel({
-//       name: name,
-//       email: email,
-//       password: hashedPassword,
-
-//     });
-
-//     console.log("newUser :>> ", newUser);
-
-//     const savedUser =  newUser.save()
-//     res.status(201).json({
-//       message: "New user registered",
-//       user: savedUser
-//     });
-//     console.log('savedUser :>> ', savedUser);
-//   } catch (error) {
-//     console.log("error saving user :>> ", error);
-//     res.status(500).json({
-//       message: "something went wrong when registering your user",
-//     });
-//   }
-// }
-
-// } catch (error) {
- 
-//   console.log("error :>> ", error);
-//   res.status(500).json({
-//     message: "Oh no, it went wrong!",
-//   });
-// }
-  
-
-// }
-
 const registerNewUser = async (req, res) => {
   const client = await dbConnection();
-
-  // await client.connect();
-
-  // const db = client.db('lilu2')
 
 try {
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,13 +1,89 @@
-import React from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import registerNewUser from "./api/registerNewUser";
 
 const SignUp = () => {
+  const [newUser, setNewUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const router = useRouter();
+  const handleRegisterInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // const { email, password } = newUser;
+    // if (!email.includes("@") && password.length < 6) {
+    //   alert(
+    //     "Your email seems to be invalid. \n Your password should be at least 6 characters"
+    //   );
+    //   return;
+    // } else if (password.length < 6) {
+    //   alert("Your password should be at least 6 characters");
+    //   return;
+    // } else if (!email.includes("@")) {
+    //   alert("Your email seems to be invalid");
+    //   return;
+    // }
+
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+      const urlencoded = new URLSearchParams();
+
+      urlencoded.append("name", newUser.name);
+      urlencoded.append("email", newUser.email);
+      urlencoded.append("password", newUser.password);
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: urlencoded,
+      };
+
+      try {
+        const response = await fetch("/api/registerNewUser", requestOptions);
+        console.log("response :>> ", response);
+        if (response.ok) {
+          const result = await response.json();
+          console.log("result in register:>> ", result);
+
+          setNewUser(result);
+          // alert("Thank you for joining Lilu!");
+          // await signIn("credentials", {
+          //   ...newUser,
+          //   redirect: false,
+          // });
+        }
+        // await router.push("../profiles");
+        // location.reload();
+      } catch (error) {
+        console.log("error in your /signup fetch:>> ", error);
+      }
+    } catch (error) {
+      console.log("error when adding new user:>> ", error);
+    }
+  };
+
+  useEffect(() => {
+    setNewUser(newUser);
+  }, []);
+
   return (
     <div className="bg-red-50 p-8">
       <div className="bg-gray-50 min-h-screen flex items-center justify-center p-4">
         <div className="bg-white shadow-md rounded-lg p-8 max-w-lg w-full">
           <h2 className="text-2xl font-bold text-gray-800 mb-6">Sign up</h2>
-          <form action="submit">
+          <form onSubmit={handleRegister} action="submit">
             <input
+              onChange={handleRegisterInput}
               placeholder="Name"
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               type="text"
@@ -18,19 +94,33 @@ const SignUp = () => {
             <br />
 
             <input
-              placeholder="Password"
+              onChange={handleRegisterInput}
+              placeholder="Email"
               className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               type="text"
-              name="password"
+              name="email"
+              required
             />
+
+            <br />
+            <br />
+
+            <input
+              placeholder="Password"
+              onChange={handleRegisterInput}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              type="password"
+              name="password"
+              required
+            />
+            <br />
+            <button
+              className="flex-1 bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              type="submit"
+            >
+              Submit
+            </button>
           </form>
-          <br />
-          <button
-            className="flex-1 bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            type="submit"
-          >
-            Submit
-          </button>
         </div>
       </div>
     </div>
